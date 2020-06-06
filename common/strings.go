cd4d6094581a216545943558d6c0e1fa3f3c8190
Matt Jones <urlugal@gmail.com>
2020-06-06 16:27:06 -0400

    tests and docs for strings

--- common/strings.go
+++ common/strings.go
@@ -6,15 +6,26 @@ import (
 	"strings"
 )
 
+// NewlineRegex is a compiled regex to detect newlines
 var NewlineRegex = regexp.MustCompile(`\r?\n`)
 
+// Pluralize will return either the singular or pluralized string
 func Pluralize(count int, singular string, plural string) string {
+
+	if count < 0 {
+		count = 0
+	}
+
 	if count == 1 {
 		return singular
 	}
+
 	return plural
 }
 
+
+// TruncateString will cut a string to the length give in the function argument and return
+// the cut string with an elipse
 func TruncateString(str string, maxLength int) string {
 	str = NewlineRegex.ReplaceAllString(str, " ")
 	str = strings.TrimSpace(str)
