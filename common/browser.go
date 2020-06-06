fb1053b9795b848e38db4a63945d88e36fb5cc09
Matt Jones <urlugal@gmail.com>
2020-06-06 17:59:57 -0400

    tests and docstrings for the browser functions

--- common/browser.go
+++ common/browser.go
@@ -5,8 +5,10 @@ import (
 	"strings"
 )
 
+// UserAgent will store the proper agent
 var UserAgent = fmt.Sprintf("%s v%s", Name, Version)
 
+// CleanUrlSpaces will replaces spaces in a string with dashes so that they can be used in urls
 func CleanUrlSpaces(dirtyStrings ...string) []string {
 	var result []string
 	for _, s := range dirtyStrings {
