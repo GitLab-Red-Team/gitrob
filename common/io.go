34982c436edacda2b1969cb6e3278752b0fd9622
Matt Jones <urlugal@gmail.com>
2020-06-06 18:00:24 -0400

    add silly error checking

--- common/io.go
+++ common/io.go
@@ -1,11 +1,19 @@
 package common
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 // FileExists will check if a file exists and return a bool based on the result
 func FileExists(path string) bool {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
+		if err != nil {
+			fmt.Println("The file could not be checked")
+			// TODO Handle the error msg
+		}
 		return false
 	}
+
 	return true
 }
